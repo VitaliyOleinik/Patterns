042d0edf84bc692e74954cfe7aa97b95f5b46f55
VitaliyOleinik <oleynik624@gmail.com>
2021-08-05 14:47:01 +0600

    example of using BUILDER PATTERN with Car

---
+++ Builder/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        CarBuilder builder = new ModernCarBuilder();
+        AutomotiveEngineer engineer = new AutomotiveEngineer(builder);
+        Car car = engineer.manufactureCar();
+        if (car != null) {
+            System.out.println("Below car delievered: ");
+            System.out.println("======================================================================");
+            System.out.println(car);
+            System.out.println("======================================================================");
+        }
+    }
+}
